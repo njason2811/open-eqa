5ee25e0fcb6c32bd52baafb40de6e945e5d53567
njason2811 <143457060+njason2811@users.noreply.github.com>
2025-09-30 17:47:01 -0500

    Update gemini-2.5-flash.py

--- openeqa/baselines/gemini-2.5-flash.py
+++ openeqa/baselines/gemini-2.5-flash.py
@@ -44,7 +44,7 @@ def parse_args() -> argparse.Namespace:
     args = parser.parse_args()
     args.output_directory.mkdir(parents=True, exist_ok=True)
     args.output_path = args.output_directory / (
-        args.dataset.stem + "-{}.json".format(args.model)
+        args.dataset.stem + "-{}-blind.json".format(args.model)
     )
     return args
 
